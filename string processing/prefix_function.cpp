334f66c38203fb657cf682a2b704e67a5935c5d6
Levichev <rodion.levichev@yandex.ru>
2020-03-13 20:52:36 +0300

    origin master

---
+++ string processing/prefix_function.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> prefix_function(string s){
+    int n = s.size();
+    vector<int> a(n);
+
+    for(int i = 1; i < n; ++i){
+        int j = a[i-1];
+        while(j > 0 && s[i] != s[j])
+            j = a[j-1];
+        if (s[i] == s[j]) ++j;
+        a[i] = j;
+    }
+    return a;
+}
+
+int main(){
+    string s; cin >> s;
+
+    vector<int> a = prefix_function(s);
+
+    return 0;
+}
