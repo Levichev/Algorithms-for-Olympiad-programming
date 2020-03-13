932b4ef03c623278a32960aeb57c1c999e4e7e97
Levichev <rodion.levichev@yandex.ru>
2020-03-13 17:42:35 +0300

    https://github.com/Levichev/Algorithms-for-Olympiad-programming.git

---
+++ snippets/the initial snippet.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define llmin LONG_LONG_MIN
+#define llmax LONG_LONG_MAX
+#define IMIN INT_MIN
+#define IMAX INT_MAX
+#define ff first
+#define ss second
+#define pb push_back
+#define ll long long
+#define all(a) a.begin(),a.end()
+#define b(v) v.begin()
+#define e(v) v.end()    
+#define rep(i,a,b) for(auto i=(a); i<(b); i++)
+#define dec(i,n,a) for(auto i=(n); i>=(a); i--)
+#define sqr(a) ((a)*(a))
+#define endl '\n'
+#define nl '\n'
+#define sz(s) s.size()
+#define len(s) s.length()
+#define inp(a) for(auto& i: a) cin >> i
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+//#define _DEBUG
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+    #ifdef _DEBUG
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    return 0;
+}
