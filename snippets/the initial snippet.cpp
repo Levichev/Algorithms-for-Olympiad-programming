cdfa8b0b9034e010ffec85cbc93d92628ab14550
Levichev <rodion.levichev@yandex.ru>
2020-06-05 14:56:29 +0300

    Update from 05.06.20

--- snippets/the initial snippet.cpp
+++ snippets/the initial snippet.cpp
@@ -1,40 +1,54 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-#define llmin LONG_LONG_MIN
-#define llmax LONG_LONG_MAX
+#define LLMIN LONG_LONG_MIN
+#define LLMAX LONG_LONG_MAX
 #define IMIN INT_MIN
 #define IMAX INT_MAX
+#define ff first
+#define ss second
+#define pb push_back
 #define all(a) a.begin(),a.end()
 #define rep(i,a,b) for(auto i=(a); i<(b); i++)
 #define dec(i,n,a) for(auto i=(n); i>=(a); i--)
-#define sqr(a) ((a)*(a))
-#define endl '\n'
 #define nl '\n'
 #define sz(s) s.size()
 #define len(s) s.length()
 #define inp(a) for(auto& i: a) cin >> i
+#define prt(a) for(auto& i: a) cout << i << ' '
+#define _ auto
 #define Yes cout << "Yes" << endl
 #define No cout << "No" << endl
-
-typedef long long ll;
-typedef pair<int,int> pii;
-typedef vector<int> vi;
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define inc int t_; cin >> t_; while(t_--)
+typedef int64_t ll;
+typedef uint64_t ull;
 typedef vector<bool> vb;
+typedef map<int,int> mii;
+typedef map<string,int> msi;
+typedef pair<int32_t,int32_t> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int32_t> vi;
 typedef vector<ll> vll;
-typedef vector<vector<int>> vvi;
-
+typedef vector<string> vs;
+typedef vector<vector<int32_t>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<string>> vvs;
 //#define _DEBUG
+ 
+void solve() {
+    
+}
 
-
-int main()
-{
-    ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0); 
     #ifdef _DEBUG
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
+
+    inc solve();
+    
     
     return 0;
 }
