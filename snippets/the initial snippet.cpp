556efe3df305aeb30f132ff93abe041cd273f773
Levichev <rodion.levichev@yandex.ru>
2021-03-11 23:21:45 +0300

    update snippet

--- snippets/the initial snippet.cpp
+++ snippets/the initial snippet.cpp
@@ -1,54 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define LLMIN LONG_LONG_MIN
-#define LLMAX LONG_LONG_MAX
-#define IMIN INT_MIN
-#define IMAX INT_MAX
-#define ff first
-#define ss second
-#define pb push_back
-#define all(a) a.begin(),a.end()
-#define rep(i,a,b) for(auto i=(a); i<(b); i++)
-#define dec(i,n,a) for(auto i=(n); i>=(a); i--)
-#define nl '\n'
-#define sz(s) s.size()
-#define len(s) s.length()
-#define inp(a) for(auto& i: a) cin >> i
-#define prt(a) for(auto& i: a) cout << i << ' '
-#define _ auto
-#define Yes cout << "Yes" << endl
-#define No cout << "No" << endl
-#define YES cout << "YES" << endl
-#define NO cout << "NO" << endl
-#define inc int t_; cin >> t_; while(t_--)
-typedef int64_t ll;
-typedef uint64_t ull;
-typedef vector<bool> vb;
-typedef map<int,int> mii;
-typedef map<string,int> msi;
-typedef pair<int32_t,int32_t> pii;
-typedef pair<ll,ll> pll;
-typedef vector<int32_t> vi;
-typedef vector<ll> vll;
-typedef vector<string> vs;
-typedef vector<vector<int32_t>> vvi;
-typedef vector<vector<ll>> vvll;
-typedef vector<vector<string>> vvs;
+#define     INF                 INT_MAX
+#define     INFLL               LONG_LONG_MAX
+#define     F                   first
+#define     S                   second
+#define     pb                  push_back
+#define     all(a)              a.begin(),a.end()   
+#define     rep(i,a,b)          for(auto i=(a); i<(b); i++)
+#define     dec(i,n,a)          for(auto i=(n); i>=(a); i--)
+#define     each(a,x)           for (auto& a: x)
+#define     nl                  '\n'
+#define     sz(s)               ((int)(s.size())
+#define     inp(a)              for(int i_=0;i_<a.size();i_++)cin>>a[i_] 
+#define     prt(a)              for(int i_=0;i_<a.size();i_++)cout << a[i_] << ' '
+#define     _                   auto
+#define     Yes                 cout << "Yes" << nl
+#define     No                  cout << "No" << nl
+#define     YES                 cout << "YES" << nl
+#define     NO                  cout << "NO" << nl
+#define     inc                 int t_; cin >> t_; while(t_--)
+#define     vec                 vector
+typedef     __int128_t          ll128;
+typedef     long long           ll;
+typedef     string              str;
+typedef     long double         ld;
+typedef     uint64_t            ull;
+typedef     vec<bool>           vb;
+typedef     map<int,int>        mii;
+typedef     pair<int,int>       pii;
+typedef     pair<ll,ll>         pll;
+typedef     vec<int>            vi;
+typedef     vec<ll>             vll;
+typedef     vec<str>            vs;
+typedef     vec<vec<int>>       vvi;
+typedef     vec<vec<ll>>        vvll;
+typedef     vec<vec<str>>       vvs;
 //#define _DEBUG
- 
+     
 void solve() {
-    
+
 }
 
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0); 
+    ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
     #ifdef _DEBUG
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
-
     inc solve();
-    
-    
     return 0;
 }
